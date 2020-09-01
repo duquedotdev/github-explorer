de7faf6dc23e0e4ef1e9a0585edc73d8cebce060
Felipe Duque <felipe@duque.dev>
2020-09-01 06:37:29 -0300

    feat: dashboard stytle done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+    <Repositories>
+      <a href="teste">
+        <img src="https://avatars1.githubusercontent.com/u/57725054?s=460&u=75adfd78047f87ae96404beefa6339445539ac53&v=4" alt="Felipe Duque" />
+        <div>
+          <strong>duquedotdev/Dumont</strong>
+          <p>Sistema open-source de gerenciamento inteligente de escola de aviação.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img src="https://avatars1.githubusercontent.com/u/57725054?s=460&u=75adfd78047f87ae96404beefa6339445539ac53&v=4" alt="Felipe Duque" />
+        <div>
+          <strong>duquedotdev/Dumont</strong>
+          <p>Sistema open-source de gerenciamento inteligente de escola de aviação.</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+
+  </>
+);
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import githubBackgroung from '../assets/github-background.svg';
 
 export default createGlobalStyle`
 *{
@@ -9,11 +10,21 @@ export default createGlobalStyle`
 }
 
 body { 
-  background: #f0f0f5;
+  background: #f0f0f5 url(${githubBackgroung}) no-repeat 70% top;
   --webkit-font-smoothing: antialiased;
 }
 
 body, input, button {
-  font: 16px sans-serif;
+  font: 16px Roboto,sans-serif;
+}
+
+#root {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 40px 20px;
+}
+
+button {
+  cursor: pointer;
 }
 `;
